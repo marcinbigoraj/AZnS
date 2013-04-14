265bc0fd81c7f07a27fa23c7b20661e6e66d8e75
Marcin Bigoraj <marcinbigoraj@gmail.com>
2013-04-14 15:00:24 +0200

    Kontroler i widok lista

---
+++ application/controllers/lista.php
@@ -0,0 +1,20 @@
+<?php
+
+class Lista extends CI_Controller
+{
+
+	public function __construct() 
+	{
+		parent::__construct();
+	}
+	
+	public function lista()
+	{
+		redirect('main/index');
+		
+				
+	}
+
+}
+
+?>
