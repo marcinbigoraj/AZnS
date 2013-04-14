101ec3d40447296584cbb1a8e65905c959aa27b4
EviQ <narejko.ewelina@pollub.edu.pl>
2013-04-14 16:12:18 +0200

    skrypty

--- application/sql/search.sql
+++ application/sql/search.sql
@@ -1,20 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas wygenerowania: 14 Kwi 2013, 16:10
+-- Wersja serwera: 5.5.27
+-- Wersja PHP: 5.4.7
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Baza danych: `kamil123_azns`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_cat` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  PRIMARY KEY (`id_cat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `found_auctions`
+--
+
+CREATE TABLE IF NOT EXISTS `found_auctions` (
+  `id_user` int(11) NOT NULL,
+  `id_auc` int(11) NOT NULL,
+  PRIMARY KEY (`id_user`,`id_auc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `search`
+--
 
 CREATE TABLE IF NOT EXISTS `search` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `keywords` varchar(60) NOT NULL,
-  `id_cat` int(11) NOT NULL,
+  `id_cat` int(11) DEFAULT NULL,
   `buyNow` tinyint(1) NOT NULL,
-  `city` varchar(35) NOT NULL,
-  `voivodeship` int(11) NOT NULL,
+  `city` varchar(35) DEFAULT NULL,
+  `voivodeship` int(11) DEFAULT NULL,
   `minPrice` float NOT NULL,
   `maxPrice` float NOT NULL,
   `active` tinyint(1) NOT NULL,
