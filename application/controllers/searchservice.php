95da6dd130950793022bb62fa4c85fce9329bfb6
Kamil <limak10101@gmail.com>
2013-04-15 20:56:14 +0200

    Wysylanie maili - phpmailer

--- application/controllers/searchservice.php
+++ application/controllers/searchservice.php
@@ -6,127 +6,17 @@ class SearchService extends CI_Controller {
 		parent::__construct();
 	}
 
-	public function search() {
-		$this -> load -> library('allegrowebapisoapclient');
-		$this -> load -> helper('email');
-		
-		// $sendData = array(
-// 			
-			// 'telefon' => '',
-			// 'email' => '',
-// 			
-			// aukcja => array(
-				// //dane aukcji
-			// )
-// 		
-		// );
-
-		$currentUserId = $this -> ion_auth -> user() -> row() -> id;
-		$query = $this -> db -> query("SELECT * FROM search WHERE active = 1 AND user_id=$currentUserId");
-
-		$data['title'] = "Wyszukane aukcje";
-		$data['searchArray'] = array();
-
-		foreach ($query->result() as $row) {
-
-			$keyword = $row -> keywords;
-			$catId = $row -> id_cat;
-			$buyNow = $row -> buyNow;
-			$city = $row -> city;
-			$state = $row -> voivodeship;
-			$minPrice = $row -> minPrice;
-			$maxPrice = $row -> maxPrice;
-
-			$offset = 0;
-			$limit = 100;
-
-			try {
-				do {
-
-					$allegroResult = $this -> allegrowebapisoapclient -> searchAuction($keyword, $buyNow, $catId, $offset, $city, $state, $minPrice, $maxPrice, $limit);
-					if ($allegroResult -> searchCount != 0) {
-						foreach ($allegroResult -> searchArray -> item as $item) {
-
-							$query = $this -> db -> query("SELECT * FROM found_auctions WHERE id_user=$currentUserId AND id_auc=$item->sItId");
-							if ($query -> num_rows() == 0) {
-								array_push($data['searchArray'], $item);
-
-							}
-
-						}
-
-						$offset += $limit;
-					}
-
-				} while ($offset < $allegroResult->searchCount);
-			} catch(Exception $exception ) {
-
-			}
-		}
-
-		if (!empty($data['searchArray'])) {
-			//przydałoby się tutaj ignorowanie błędów bazy
-			foreach ($data['searchArray'] as $item) {
-
-				$this -> db -> query("INSERT INTO found_auctions VALUES($currentUserId,$item->sItId)");
-
-			}
-
-			$message = "<table>";
-			foreach ($data['searchArray'] as $item) {
-				$message .= "<tr>";
-
-				if ($item -> sItThumb == 1) {
-					$message .= '<td rowspan="2"><img src="' . $item -> sItThumbUrl . '" alt="' . $item -> sItName . '" /></td>';
-				} else {
-					$message .= '<td rowspan="2"><img src="http://static.allegrostatic.pl/site_images/1/0/layout/showItemNoPhoto.png" alt="Brak zdjęcia" /></td>';
-				}
-
-				$message .= '<td>' . $item -> sItId . ' - ' . $item -> sItName . '</td>';
-				$message .= '<td>';
-				$message .= 'Aktualna cena: ' . $item -> sItPrice . '<br />';
-
-				if ($item -> sItIsBuyNow != 0) {
-					$message .= 'Cena kup teraz: ' . $item -> sItIsBuyNow;
-				}
-
-				$message .= '</td></tr><tr>';
-				$message .= '<td>Pozostało: ' . $item -> sItTimeLeft . ' | Kategoria: ' . $item -> sItCategoryId . '</td>';
-				$message .= '<td>Locaklizacja: ' . $item -> sItCity . ' | ' . $item -> sItCountry . '</td></tr>';
-			}
-
-			$message .= "</table>";
-
-			$query = $this -> db -> query("SELECT email FROM users WHERE id=$currentUserId");
-
-			if ($query -> num_rows() > 0) {
-				$row = $query -> row();
-
-				$mail = $row -> email;
-				$subject = "Powiadomienie z dnia " . date("Y-m-d H:i:s");
-				mail($mail, $subject, $message);
-			}
-		}
-
-		$this -> load -> view('templates/header', $data);
-		$this -> load -> view('search_items_view', $data);
-		$this -> load -> view('templates/footer');
-
-	}
-
-
 	public function searchAllNews() {
 		
-		$this -> load -> library('allegrowebapisoapclient');		
-
+		$this->load->library('allegrowebapisoapclient');	
+		$this->load->library('phpmailer/phpmailer');	
+		
 		$filterQuery = $this->db->query("
 			SELECT u.username, u.email, u.phone, s.* FROM search s
 			INNER JOIN users u ON u.id = s.user_id
 			WHERE s.active = 1
 		");
 
-		$data['title'] = "Wyszukane aukcje";
-		$data['searchArray'] = array();
 		$searchedData = array();
 
 		foreach ($filterQuery->result() as $row) 
@@ -171,9 +61,6 @@ class SearchService extends CI_Controller {
 								if (!isset($searchedData[$userId]))
 								{
 									$searchedData[$userId] = array(
-										'username' => $username,
-										'email' => $email,
-										'phone' => $phone,
 										'auctions' => array(),
 										'filter' => $row
 									);
@@ -181,7 +68,7 @@ class SearchService extends CI_Controller {
 								
 								array_push($searchedData[$userId]['auctions'], $item);	
 								
-							//	$this->db->query("INSERT INTO found_auctions VALUES($userId,$item->sItId)");
+								//$this->db->query("INSERT INTO found_auctions VALUES($userId,$item->sItId)");
 															
 							}
 
@@ -203,27 +90,42 @@ class SearchService extends CI_Controller {
 		}
 
 		$this->sendSearchedData($searchedData);	
-	
-		$this -> load -> view('templates/header', $data);
-		$this -> load -> view('search_items_view', $data);
-		$this -> load -> view('templates/footer');
 
 	}
 
 	private function sendSearchedData($searchedData)
 	{
-		$this -> load -> helper('email');
-		
+
 		foreach ($searchedData as $user)
 		{
-			$username = $user['username'];
-			$email = $user['email'];
-			$phone = $user['phone'];
+			$username = $user['filter']->username;
+			$email = $user['filter']->email;
+			$phone = $user['filter']->phone;
+			
+			$keyword = $user['filter']->keywords;
+			$catId = $user['filter']->id_cat;
+			$buyNow = $user['filter']->buyNow;
+			$city = $user['filter']->city;
+			$state = $user['filter']->voivodeship;
+			$minPrice = $user['filter']->minPrice;
+			$maxPrice = $user['filter']->maxPrice;
 			
 			if ($user['auctions'] > 0) 
 			{
 				
-				$message = "<table>";
+				$message = '<table><tr>';
+				
+				$message .= '<td>'.$keyword.'</td>';
+				$message .= '<td>'.$catId.'</td>';
+				$message .= '<td>'.$buyNow.'</td>';
+				$message .= '<td>'.$city.'</td>';
+				$message .= '<td>'.$state.'</td>';
+				$message .= '<td>'.$minPrice.'</td>';
+				$message .= '<td>'.$maxPrice.'</td>';
+				
+				$message .= '</table></tr>';
+				
+				$message .= "<table>";
 				
 				foreach ($user['auctions'] as $auction)
 				{					
@@ -232,11 +134,11 @@ class SearchService extends CI_Controller {
 	
 					if ($auction->sItThumb == 1) 
 					{
-						$message .= '<td rowspan="2"><img src="' . $auction->sItThumbUrl . '" alt="' . $auction->sItName . '" /></td>';
+						$message .= '<td rowspan="2"><img src="' . $auction->sItThumbUrl . '" alt="' . $auction->sItName . '" style="width: 100px; height: auto;" /></td>';
 					} 
 					else 
 					{
-						$message .= '<td rowspan="2"><img src="http://static.allegrostatic.pl/site_images/1/0/layout/showItemNoPhoto.png" alt="Brak zdjęcia" /></td>';
+						$message .= '<td rowspan="2"><img src="http://static.allegrostatic.pl/site_images/1/0/layout/showItemNoPhoto.png" alt="Brak zdjęcia" style="width: 100px; height: auto;" /></td>';
 					}
 	
 					$message .= '<td><a href="http://testwebapi.pl/show_item.php?item='.$auction->sItId.'">' . $auction->sItId . ' - ' . $auction->sItName . '</a></td>';
@@ -255,10 +157,34 @@ class SearchService extends CI_Controller {
 				}
 	
 				$message .= "</table>";
-				
-				echo $message;
+				$title = "Allegro Search Service z dnia " . date("H:i:s d-m-Y"); 
+				$this->mailSender($title, $message, $email);
 					
 			}
+		}
+		
+	}
+
+	private function mailSender($Subject, $BodyHtml, $To)
+	{
+		
+		$this->phpmailer->IsSMTP();
+		$this->phpmailer->SMTPAuth = true;
+		$this->phpmailer->SMTPSecure = 'tls';	
+		$this->phpmailer->Host = 'smtp.gmail.com';
+		$this->phpmailer->Post = 587;
+		$this->phpmailer->Username = 'kamilsitetk@gmail.com';
+		$this->phpmailer->Password = '';
+		$this->phpmailer->From = 'kamilsitetk@gmail.com';
+		$this->phpmailer->FromName = "Allegro Search Service";
+		$this->phpmailer->Subject = $Subject;
+		$this->phpmailer->AltBody = "Konieczne wsparcie HTML";
+		$this->phpmailer->MsgHTML($BodyHtml);
+		$this->phpmailer->AddAddress($To);
+		
+		if (!$this->phpmailer->Send())
+		{
+			echo 'Mailer Error: ' . $this->phpmailer->ErrorInfo;
 		}
 		
 	}
