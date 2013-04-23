f764a1f1ea2c5e6853fdbaeb3039f442cd6e9e93
Kamil <limak10101@gmail.com>
2013-04-23 19:18:32 +0200

    Run service

--- run_service.php
+++ run_service.php
@@ -4,6 +4,7 @@ $system_path = 'system';
 define('BASEPATH', str_replace("\\", "/", $system_path));
 require_once('application/config/config.php');
 
+file_get_contents($config['base_url'].'index.php/basicdataservice/removeEndedAuctions');
 file_get_contents($config['base_url'].'index.php/basicdataservice/getNewVersion');
 file_get_contents($config['base_url'].'index.php/searchservice/searchallnews');
 
